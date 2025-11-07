6cdcc8335d3e7dfe1e0af10f63cef3d5d5b7b12c
GK Malinda <164718081+krishaCode@users.noreply.github.com>
2025-11-07 22:55:29 +0530

    first commit

---
+++ Backend/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MongoDB_URI, ).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
